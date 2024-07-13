f80f23867d70687e47fd2f9d7a4f3c450489718f
GreedyGaurav <135502383+GreedyGaurav@users.noreply.github.com>
2024-07-13 19:29:09 +0530

    Update firebase.js

--- TrainTrippers-main/firebase.js
+++ TrainTrippers-main/firebase.js
@@ -1,21 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA71yeaqMjx6_aXnqAfq9RkPqxI2o2tms0",
-  authDomain: "traintrippers-bdb24.firebaseapp.com",
-  projectId: "traintrippers-bdb24",
-  storageBucket: "traintrippers-bdb24.appspot.com",
-  messagingSenderId: "253922297321",
-  appId: "1:253922297321:web:1f3692e66911b3c8953778",
-  measurementId: "G-484QLEEFQS"
-};
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBjCKOY_zNL0irmAM5O7DA4PhHDuKr0YCY",
+    authDomain: "easy-journey-b5e9b.firebaseapp.com",
+    databaseURL: "https://easy-journey-b5e9b-default-rtdb.firebaseio.com",
+    projectId: "easy-journey-b5e9b",
+    storageBucket: "easy-journey-b5e9b.appspot.com",
+    messagingSenderId: "59337509697",
+    appId: "1:59337509697:web:bc3b82f3f044473ab36fc0",
+    measurementId: "G-GQE4VTXDTL"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  export default app;
+// const auth =getAuth();
