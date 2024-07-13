2179ad58319b523b81639b49df56fd941aeb3411
GreedyGaurav <135502383+GreedyGaurav@users.noreply.github.com>
2024-07-13 19:13:47 +0530

    Update firebase.js

--- TrainTrippers-main/firebase.js
+++ TrainTrippers-main/firebase.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,14 +8,13 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyBjCKOY_zNL0irmAM5O7DA4PhHDuKr0YCY",
-    authDomain: "easy-journey-b5e9b.firebaseapp.com",
-    databaseURL: "https://easy-journey-b5e9b-default-rtdb.firebaseio.com",
-    projectId: "easy-journey-b5e9b",
-    storageBucket: "easy-journey-b5e9b.appspot.com",
-    messagingSenderId: "59337509697",
-    appId: "1:59337509697:web:bc3b82f3f044473ab36fc0",
-    measurementId: "G-GQE4VTXDTL"
+    apiKey: "AIzaSyA71yeaqMjx6_aXnqAfq9RkPqxI2o2tms0",
+    authDomain: "traintrippers-bdb24.firebaseapp.com",
+    projectId: "traintrippers-bdb24",
+    storageBucket: "traintrippers-bdb24.appspot.com",
+    messagingSenderId: "253922297321",
+    appId: "1:253922297321:web:1f3692e66911b3c8953778",
+    measurementId: "G-484QLEEFQS"
   };
 
   // Initialize Firebase
